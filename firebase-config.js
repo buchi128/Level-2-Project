95503d3b391914e8c09daf71d8cbb74346e81432
buchi128 <josephisu4@gmail.com>
2026-08-27 16:03:26 +0100

    Refactor: Integrated firebase-config module and removed bundler env dependencies

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDewnlmrqgHffKSID_XtQRTc7aTRRDloic",
+  authDomain: "://firebaseapp.com",
+  databaseURL: "https://firebaseio.com",
+  projectId: "my-eventswhatsup",
+  storageBucket: "my-eventswhatsup.firebasestorage.app",
+  messagingSenderId: "82839194346",
+  appId: "1:82839194346:web:a4813a773ee5396dd26593",
+  measurementId: "G-5ZZ236N3BE"
+};
